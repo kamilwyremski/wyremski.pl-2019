015a8d274179a63d04856ae7a268bdc9db1973ee
Kamil Wyremski <kamil.wyremski@gmail.com>
2019-12-09 16:12:45 -0500

    Konfiguracja service worker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { hydrate, render } from "react-dom";
 import App from './App';
 import ReactGA from 'react-ga';
+import * as serviceWorker from './serviceWorker';
+
 ReactGA.initialize('UA-46218779-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
@@ -11,6 +13,8 @@ if (rootElement.hasChildNodes()) {
 } else {
   render(<App />, rootElement);
 }
+
+serviceWorker.register();
 
 window.snapSaveState = () => {
   document.querySelector("#root").setAttribute("data-server-rendered", "true");
