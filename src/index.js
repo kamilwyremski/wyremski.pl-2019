35541e6c75770f3665f58a89e6d9c3646d0115aa
Kamil Wyremski <kamil.wyremski@gmail.com>
2021-06-04 15:56:37 -0500

    Usunięcie google analytics

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { hydrate, render } from "react-dom";
 import App from './App';
-import ReactGA from 'react-ga';
 import * as serviceWorker from './serviceWorker';
-
-ReactGA.initialize('UA-46218779-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 const rootElement = document.getElementById("root");
 if (rootElement.hasChildNodes()) {
