24343b4586f621cac373a25923288b56ec5d22ff
Kamil Wyremski <kamil.wyremski@gmail.com>
2024-03-07 20:45:45 -0500

    Add hrefLang

--- src/App.js
+++ src/App.js
@@ -1,123 +1,174 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AnimatePresence } from "framer-motion"; // Importuj AnimatePresence
 
-import './App.scss';
+import "./App.scss";
 
-import {IntlProvider} from "react-intl";
-import { messages } from './Lang';
+import { IntlProvider } from "react-intl";
+import { messages } from "./Lang";
 
-import Meta from './components/Meta/Meta';
-import Nav from './components/Nav/Nav';
-import Home from './components/Home/Home';
-import Scripts from './components/Scripts/Scripts';
-import Script from './components/Script/Script';
-import Projects from './components/Projects/Projects';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
-import Error404 from './components/Error404/Error404';
+import Meta from "./components/Meta/Meta";
+import Nav from "./components/Nav/Nav";
+import Home from "./components/Home/Home";
+import Scripts from "./components/Scripts/Scripts";
+import Script from "./components/Script/Script";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import Error404 from "./components/Error404/Error404";
 
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      language: 'pl',
-      component: 'home',
-      alternate_slug: '',
+      language: "pl",
+      component: "home",
+      alternate_slug: "",
       messages: messages,
-      meta: ''
-    }
+      meta: "",
+    };
   }
 
   componentDidMount() {
-    window.addEventListener('scroll', this.doAnimations);
+    window.addEventListener("scroll", this.doAnimations);
     this.doAnimations();
   }
 
-  handleLanguage(lang, component='home', data={}) {
-    if(data.messages !== undefined){
-      let m = {}
-      m.pl = {...messages.pl, ...data.messages.pl }
-      m.en = {...messages.en, ...data.messages.en }
+  handleLanguage(lang, component = "home", data = {}) {
+    if (data.messages !== undefined) {
+      let m = {};
+      m.pl = { ...messages.pl, ...data.messages.pl };
+      m.en = { ...messages.en, ...data.messages.en };
       this.setState({
-        messages: m
-      })
+        messages: m,
+      });
     }
 
     this.setState({
       language: lang,
       component: component,
-      alternate_slug: data.script_name || '',
-      meta: data
-    })
+      alternate_slug: data.script_name || "",
+      meta: data,
+    });
   }
 
-  componentDidUnMount(){
-    window.removeEventListener('scroll', this.doAnimations);
+  componentDidUnMount() {
+    window.removeEventListener("scroll", this.doAnimations);
   }
 
   doAnimations = () => {
     let offset = document.documentElement.scrollTop + window.innerHeight;
-    document.querySelectorAll(".animatable").forEach(animatable => {
-      if((animatable.offsetTop + 80) < offset) {
+    document.querySelectorAll(".animatable").forEach((animatable) => {
+      if (animatable.offsetTop + 80 < offset) {
         animatable.classList.remove("animatable");
         animatable.classList.add("animated");
       }
-    })
+    });
   };
-  
-  createRoute(t){
-    let routes = []
-    Object.keys(messages).forEach(function(lang, index){
+
+  createRoute(t) {
+    let routes = [];
+    Object.keys(messages).forEach(function (lang, index) {
       routes.push(
-        <Route path={messages[lang]['nav.link.home']} key={index+lang+'home'} element={
-          <Home language={lang} handleLanguage = {t.handleLanguage.bind(t)}/> } 
+        <Route
+          path={messages[lang]["nav.link.home"]}
+          key={index + lang + "home"}
+          element={
+            <Home language={lang} handleLanguage={t.handleLanguage.bind(t)} />
+          }
         />
-      )
+      );
       routes.push(
-        <Route path={messages[lang]['nav.link.scripts']} key={index+lang+'scripts'} element={
-          <Scripts language={lang} handleLanguage = {t.handleLanguage.bind(t)}/> } 
+        <Route
+          path={messages[lang]["nav.link.scripts"]}
+          key={index + lang + "scripts"}
+          element={
+            <Scripts
+              language={lang}
+              handleLanguage={t.handleLanguage.bind(t)}
+            />
+          }
         />
-      )
+      );
       routes.push(
-        <Route path={messages[lang]['nav.link.script']+"/:script_slug"} key={index+lang+'script'} element={
-          <Script language={lang} handleLanguage = {t.handleLanguage.bind(t)}/> } 
+        <Route
+          path={messages[lang]["nav.link.script"] + "/:script_slug"}
+          key={index + lang + "script"}
+          element={
+            <Script language={lang} handleLanguage={t.handleLanguage.bind(t)} />
+          }
         />
-      )
+      );
       routes.push(
-        <Route path={messages[lang]['nav.link.projects']} key={index+lang+'projects'} element={
-          <Projects language={lang} handleLanguage = {t.handleLanguage.bind(t)}/> } 
+        <Route
+          path={messages[lang]["nav.link.projects"]}
+          key={index + lang + "projects"}
+          element={
+            <Projects
+              language={lang}
+              handleLanguage={t.handleLanguage.bind(t)}
+            />
+          }
         />
-      )
+      );
       routes.push(
-        <Route path={messages[lang]['nav.link.contact']} key={index+lang+'contact'} element={
-          <Contact language={lang} handleLanguage = {t.handleLanguage.bind(t)}/> } 
+        <Route
+          path={messages[lang]["nav.link.contact"]}
+          key={index + lang + "contact"}
+          element={
+            <Contact
+              language={lang}
+              handleLanguage={t.handleLanguage.bind(t)}
+            />
+          }
         />
-      )
-    })
+      );
+    });
     return routes;
   }
 
   render() {
-    return(
-      <IntlProvider locale={this.state.language} messages={this.state.messages[this.state.language]} key={ this.state.language }>
-        <Meta language={this.state.language} component={this.state.component} meta={this.state.meta}></Meta>
+    console.info(
+      "%cKamil Wyremski\n%cFull Stack Web Developer",
+      "font-family: Montserrat, sans-serif; font-size: 24px; font-weight: bold;",
+      "font-family: Montserrat, sans-serif; font-size: 14px;"
+    );
+
+    return (
+      <IntlProvider
+        locale={this.state.language}
+        messages={this.state.messages[this.state.language]}
+        key={this.state.language}
+      >
+        <Meta
+          language={this.state.language}
+          component={this.state.component}
+          meta={this.state.meta}
+        ></Meta>
         <Router onUpdate={() => window.scrollTo(0, 0)}>
           <div>
-            <Nav language={this.state.language} component={this.state.component} alternate_slug={this.state.alternate_slug}/>
+            <Nav
+              language={this.state.language}
+              component={this.state.component}
+              alternate_slug={this.state.alternate_slug}
+            />
             <AnimatePresence mode="wait">
-            <Routes >
-              {this.createRoute(this)}
-              <Route path="*" status={404} notFound={true} element={<Error404/>} />
-            </Routes>
+              <Routes>
+                {this.createRoute(this)}
+                <Route
+                  path="*"
+                  status={404}
+                  notFound={true}
+                  element={<Error404 />}
+                />
+              </Routes>
             </AnimatePresence>
             <Footer />
           </div>
         </Router>
       </IntlProvider>
-    )
+    );
   }
-};
+}
 
 export default App;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,81 +1,81 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link, NavLink } from "react-router-dom";
-import {injectIntl , FormattedMessage} from 'react-intl';
-import { messages } from './../../Lang';
-import './Nav.scss';
+import { injectIntl, FormattedMessage } from "react-intl";
+import { messages } from "./../../Lang";
+import "./Nav.scss";
 
 class Nav extends Component {
-  
-  constructor({intl, ...props}) {
+  constructor({ intl, ...props }) {
     super();
     this.state = {
       active_mobile_menu: false,
-      nav_top: '20px',
-      alternate_url: ''
+      nav_top: "20px",
+      alternate_url: "",
     };
   }
 
   showHideMenu = () => {
     this.setState({
-      active_mobile_menu: !this.state.active_mobile_menu
+      active_mobile_menu: !this.state.active_mobile_menu,
     });
-  }
+  };
 
   onClickMenu = (event) => {
     this.showHideMenu();
     event.currentTarget.blur();
     window.scrollTo(0, 0);
-  }
+  };
 
   componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
+    window.addEventListener("scroll", this.handleScroll);
 
     let nav_link_delay = 1;
-    document.querySelectorAll("#navbar--menu .hide").forEach(link => {
-      setTimeout(
-        function() {
-          link.classList.remove("hide");
-        }, 
-        200 * nav_link_delay
-      );
+    document.querySelectorAll("#navbar--menu .hide").forEach((link) => {
+      setTimeout(function () {
+        link.classList.remove("hide");
+      }, 200 * nav_link_delay);
       nav_link_delay++;
-    })
+    });
   }
 
   componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
+    window.removeEventListener("scroll", this.handleScroll);
   }
 
   handleScroll = () => {
     let scrollTop = window.scrollY;
-    if(scrollTop>20){
+    if (scrollTop > 20) {
       scrollTop = 0;
-    }else{
+    } else {
       scrollTop = 20 - scrollTop;
     }
     this.setState({
-      nav_top: scrollTop+'px'
+      nav_top: scrollTop + "px",
     });
-  }
+  };
 
   static getDerivedStateFromProps(props, state) {
-    if (props.language !== state.language || props.component !== state.component || props.meta !== state.meta) {
-      let alternate_url = ''
+    if (
+      props.language !== state.language ||
+      props.component !== state.component ||
+      props.meta !== state.meta
+    ) {
+      let alternate_url = "";
       let component = props.component;
-      if(props.component === 'error404'){
-        component = 'home'
+      if (props.component === "error404") {
+        component = "home";
       }
-      
-      if(props.language==='pl'){
-        alternate_url = messages['en']['nav.link.'+component];
-      }else if(props.language==='en'){
-        alternate_url = messages['pl']['nav.link.'+component];
+
+      if (props.language === "pl") {
+        alternate_url = messages["en"]["nav.link." + component];
+      } else if (props.language === "en") {
+        alternate_url = messages["pl"]["nav.link." + component];
       }
-      if(props.alternate_slug){
-        alternate_url = alternate_url+'/'+props.alternate_slug;
+      if (props.alternate_slug) {
+        alternate_url = alternate_url + "/" + props.alternate_slug;
       }
-      return{
-        alternate_url: alternate_url
+      return {
+        alternate_url: alternate_url,
       };
     }
   }
@@ -83,40 +83,117 @@ class Nav extends Component {
   render() {
     const intl = this.props.intl;
     const language = this.props.language;
-    return(
-      <nav style={{top : this.state.nav_top}} className={ this.state.nav_top==='0px' ? "navbar shadow d-print-none" : "navbar d-print-none" }>
-        <div className="container d-flex" role="navigation" aria-label={ intl.formatMessage({ id: 'nav.title.navigation' })}>
-          <Link to={ intl.formatMessage({ id: 'nav.link.home' })} className="navbar--brand" title="Kamil Wyremski - Web Designer" rel="home">Kamil Wyremski <small>Full Stack Web Developer</small></Link>
-          <button className={ this.state.active_mobile_menu ? "open navbar--burger" : "navbar--burger" }
-            onClick={this.showHideMenu} 
-            aria-controls="navbar--menu" 
-            aria-expanded={this.state.active_mobile_menu} 
-            aria-label={ intl.formatMessage({ id: 'nav.title.toogle' })}>
+    return (
+      <nav
+        style={{ top: this.state.nav_top }}
+        className={
+          this.state.nav_top === "0px"
+            ? "navbar shadow d-print-none"
+            : "navbar d-print-none"
+        }
+      >
+        <div
+          className="container d-flex"
+          role="navigation"
+          aria-label={intl.formatMessage({ id: "nav.title.navigation" })}
+        >
+          <Link
+            to={intl.formatMessage({ id: "nav.link.home" })}
+            className="navbar--brand"
+            title="Kamil Wyremski - Web Designer"
+            rel="home"
+          >
+            Kamil Wyremski <small>Full Stack Web Developer</small>
+          </Link>
+          <button
+            className={
+              this.state.active_mobile_menu
+                ? "open navbar--burger"
+                : "navbar--burger"
+            }
+            onClick={this.showHideMenu}
+            aria-controls="navbar--menu"
+            aria-expanded={this.state.active_mobile_menu}
+            aria-label={intl.formatMessage({ id: "nav.title.toogle" })}
+          >
             <span></span>
             <span></span>
             <span></span>
           </button>
-          <ul id="navbar--menu" className={ this.state.active_mobile_menu ? "open d-flex navbar--menu" : "d-flex navbar--menu" }>
+          <ul
+            id="navbar--menu"
+            className={
+              this.state.active_mobile_menu
+                ? "open d-flex navbar--menu"
+                : "d-flex navbar--menu"
+            }
+          >
             <li className="hide">
-              <NavLink to={ intl.formatMessage({ id: 'nav.link.home' })} className={({ isActive }) => "nav--link" + (isActive ? " is-active" : "")} onClick={this.onClickMenu} title={ intl.formatMessage({ id: 'nav.title.home' })} rel="home"><FormattedMessage id="nav.home"/></NavLink>
+              <NavLink
+                to={intl.formatMessage({ id: "nav.link.home" })}
+                className={({ isActive }) =>
+                  "nav--link" + (isActive ? " is-active" : "")
+                }
+                onClick={this.onClickMenu}
+                title={intl.formatMessage({ id: "nav.title.home" })}
+                rel="home"
+              >
+                <FormattedMessage id="nav.home" />
+              </NavLink>
             </li>
             <li className="hide">
-              <NavLink to={ intl.formatMessage({ id: 'nav.link.scripts' })} className={({ isActive }) => "nav--link" + (isActive ? " is-active" : "")} onClick={this.onClickMenu} title={ intl.formatMessage({ id: 'nav.title.scripts' })}><FormattedMessage id="nav.scripts"/></NavLink>
+              <NavLink
+                to={intl.formatMessage({ id: "nav.link.scripts" })}
+                className={({ isActive }) =>
+                  "nav--link" + (isActive ? " is-active" : "")
+                }
+                onClick={this.onClickMenu}
+                title={intl.formatMessage({ id: "nav.title.scripts" })}
+              >
+                <FormattedMessage id="nav.scripts" />
+              </NavLink>
             </li>
             <li className="hide">
-              <NavLink to={ intl.formatMessage({ id: 'nav.link.projects' })} className={({ isActive }) => "nav--link" + (isActive ? " is-active" : "")} onClick={this.onClickMenu} title={ intl.formatMessage({ id: 'nav.title.projects' })}><FormattedMessage id="nav.projects"/></NavLink>
+              <NavLink
+                to={intl.formatMessage({ id: "nav.link.projects" })}
+                className={({ isActive }) =>
+                  "nav--link" + (isActive ? " is-active" : "")
+                }
+                onClick={this.onClickMenu}
+                title={intl.formatMessage({ id: "nav.title.projects" })}
+              >
+                <FormattedMessage id="nav.projects" />
+              </NavLink>
             </li>
             <li className="hide">
-              <NavLink to={ intl.formatMessage({ id: 'nav.link.contact' })} className={({ isActive }) => "nav--link" + (isActive ? " is-active" : "")} onClick={this.onClickMenu} title={ intl.formatMessage({ id: 'nav.title.contact' })}><FormattedMessage id="nav.contact"/></NavLink>
+              <NavLink
+                to={intl.formatMessage({ id: "nav.link.contact" })}
+                className={({ isActive }) =>
+                  "nav--link" + (isActive ? " is-active" : "")
+                }
+                onClick={this.onClickMenu}
+                title={intl.formatMessage({ id: "nav.title.contact" })}
+              >
+                <FormattedMessage id="nav.contact" />
+              </NavLink>
             </li>
             <li className="hide">
-              <a href={ this.state.alternate_url } className="nav--link" onClick={this.changeLanguage} title={language==='pl' ? 'en' : 'pl'}>{language==='pl' ? 'en' : 'pl'}&nbsp;&#9656;</a>
+              <a
+                href={this.state.alternate_url}
+                className="nav--link"
+                onClick={this.changeLanguage}
+                title={language === "pl" ? "en" : "pl"}
+                hrefLang={language === "pl" ? "en" : "pl"}
+                rel="alternate"
+              >
+                {language === "pl" ? "en" : "pl"}&nbsp;&#9656;
+              </a>
             </li>
           </ul>
         </div>
       </nav>
-    )
+    );
   }
-};
+}
 
 export default injectIntl(Nav);
